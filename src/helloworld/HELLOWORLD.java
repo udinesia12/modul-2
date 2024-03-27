0bbb9f567aad9530ed5b382a23c98c85aa719c9a
udinesia <udinesia1204@gmail.com>
2024-03-27 16:53:32 +0700

    HELLO WORLD 202357201021

---
+++ src/helloworld/HELLOWORLD.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package helloworld;
+
+/**
+ *
+ * @author HP
+ */
+public class HELLOWORLD {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World 202357201021");
+    }
+    
+}
